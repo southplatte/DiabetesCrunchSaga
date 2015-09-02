1c9c4edd5de44acc8998fa4f14724742481be202
Billy Nab <bnab@southplattewebdesign.com>
2015-09-02 10:32:21 -0600

    adding JSON level reader

--- DiabetesCrunchSaga/Level.swift
+++ DiabetesCrunchSaga/Level.swift
@@ -13,13 +13,39 @@ let NumRows = 9
 
 class Level {
     private var cookies = Array2D<Cookie>(columns: NumColumns, rows: NumRows)
+    private var tiles = Array2D<Tile>(columns: NumColumns, rows: NumRows)
 
+    init(filename: String){
+        //1
+        if let dictionary = Dictionary<String, AnyObject>.loadJSONFromBundle(filename){
+            //2
+            if let tilesArray:  AnyObject = dictionary["tiles"]{
+                //3
+                for(row, rowArray) in enumerate(tilesArray as! [[Int]]){
+                    //4
+                    let tileRow = NumRows - row - 1
+                    //5
+                    for(column, value) in enumerate(rowArray){
+                        if value == 1{
+                            tiles[column, tileRow] = Tile()
+                        }
+                    }
+                }
+            }
+        }
+    }
 
 func cookieAtColumn(column: Int, row: Int) -> Cookie? {
     assert(column >= 0 && column < NumColumns)
     assert(row >= 0 && row < NumRows)
     return cookies[column, row]
 }
+
+    func TileAtColumn(column: Int, row: Int) -> Tile? {
+        assert(column >= 0 && column < NumColumns)
+        assert(row >= 0 && row < NumRows)
+        return tiles[column, row]
+    }
 
 func shuffle() -> Set<Cookie> {
     return createInitialCookies()
@@ -31,16 +57,18 @@ private func createInitialCookies() -> Set<Cookie> {
     // 1
     for row in 0..<NumRows {
         for column in 0..<NumColumns {
+            if tiles[column, row] != nil{
+                // 2
+                var cookieType = CookieType.random()
+                
+                // 3
+                let cookie = Cookie(column: column, row: row, cookieType: cookieType)
+                cookies[column, row] = cookie
+                
+                // 4
+                set.addElement(cookie)
+            }
             
-            // 2
-            var cookieType = CookieType.random()
-            
-            // 3
-            let cookie = Cookie(column: column, row: row, cookieType: cookieType)
-            cookies[column, row] = cookie
-            
-            // 4
-            set.addElement(cookie)
         }
     }
     return set
---
+++ DiabetesCrunchSaga/Tile.swift
@@ -0,0 +1,11 @@
+//
+//  Tile.swift
+//  DiabetesCrunchSaga
+//
+//  Created by Billy Nab on 9/2/15.
+//  Copyright (c) 2015 Sucker Punch Ltd. All rights reserved.
+//
+
+class Tile {
+    
+}
